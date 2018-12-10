ec47c64da26d0c45805d5b30830cd6df89a73ae9
floter1 <floter1@gmail.com>
2018-12-10 23:41:19 +0800

    Buddy for sale functions

--- articles/urls.py
+++ articles/urls.py
@@ -7,16 +7,17 @@ from django.conf import settings
 app_name = 'articles'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('create', views.create, name='create'),
     path('delete/<int:artId>', views.delete, name='delete'),
     path('update/<int:artId>', views.update, name='update'),
     path('header/', views.header, name='header'),
     path('footer/', views.footer, name='footer'),
     path('del_bprofile/<int:profId>', views.del_bprofile, name='del_bprofile'),
-    path('del_bprofile/<int:profId>', views.del_bprofile, name='up_bprofile'),
-    path('bsell_profile/', views.bsell_profile, name='bsell_profile'),
+    path('up_bprofile/<int:profId>', views.up_bprofile, name='up_bprofile'),
+    path('', views.bsell_profile, name='bsell_profile'),
     path('sell_friends/', views.sell_friends, name='sell_friends'),
+    path('buy_friend/<int:fsellId>', views.buy_friend, name='buy_friend'),
 
 
 
--- articles/views.py
+++ articles/views.py
@@ -6,6 +6,8 @@ from django.urls import path, include
 
 from django.views.generic import TemplateView
 
+
+
 #From Members
 #from django.views.decorators.csrf import csrf_protect
 
@@ -15,17 +17,80 @@ from django.views.generic import TemplateView
 # Create your views here.
 from .models import Articles
 from .models import Bsell
+from members.models import Members
+
+
+def buy_friend(request, fsellId): 
+    """ 
+    Get data from models.py 
+    """ 
+    
+    myfriend = Bsell.objects.get(id=fsellId)
+    myfriend2 = Members.objects.get(user_name=myfriend.user_name)
+    
+    myself = Bsell.objects.get(user_name=request.user.username)
+    myself2 = Members.objects.get(user_name=request.user.username)
+    
+    prevowner = Bsell.objects.get(user_name=myfriend.owner)
+    prevowner2 = Members.objects.get(user_name=myfriend.owner)
+    
+    share = float(myfriend2.points) * 0.01
+    growth = float(myfriend.price) * 0.02
+    cost = float(myfriend2.points) + share
+
+
+
+    if myfriend.coins is not None:
+        
+        if myself2.points > cost:
+            
+            myfriend2.points = float(myfriend2.points) + cost
+            myself2.points = float(myself2.points) - cost
+            prevowner2.points = float(prevowner2.points) + share
+            
+            myfriend.price = float(myfriend.price) + growth
+            myfriend.owner = request.user.username
+            
+            myfriend.coins = myfriend2.points            
+            myself.coins = myself2.points
+            prevowner.coins = prevowner2.points
+        
+        else:
+            return redirect('articles:sell_friends')
+        
+    else:
+        
+        myfriend.coins = 0        
+    
+    myfriend.save()
+    myfriend2.save()
+    
+    myself.save()
+    myself2.save()
+    
+    prevowner.save()
+    prevowner2.save()
+    
+    return redirect('articles:sell_friends') 
 
 
 
 def sell_friends(request):
     
-    forsale = Bsell.objects.exclude(user_name = request.user.username)
+
+    forsale = Bsell.objects.exclude(owner=request.user.username) & Bsell.objects.exclude(user_name=request.user.username)
+    myprofile = Bsell.objects.filter(user_name = request.user.username)
     
+
     
     template = "bsell_home.html"
+    context = {
+    	"forsale" : forsale, 
+    	'myprofile' : myprofile
+    	}
+
     
-    return render(request, template, {"forsale" : forsale})
+    return render(request, template, context)
 
 
 
@@ -42,12 +107,24 @@ def bsell_profile(request):
         
     else:
         bprofile_create, bprofile_list = Bsell.objects.get_or_create(user_name = request.user.username)
+        myprofile2 = Members.objects.get(user_name = request.user.username)
+        bprofile_create.coins = myprofile2.points
+
         bprofile_create.save()        
         bprofile_list = Bsell.objects.all().filter(user_name = request.user.username)
         
+#        bprofile_list = Bsell.objects.get(user_name = request.user.username)
+
+        
+        
         template = "bsell_profile.html" 
         
-        return render(request, template, {'bprofiles' : bprofile_list})  
+        context = {
+        'bprofiles' : bprofile_list	
+        	
+        	}
+        
+        return render(request, template, context)  
 
 
 def up_bprofile(request, profId): 
