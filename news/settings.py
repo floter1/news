c89e0e9a946f40e1b60bd5b648be39bc2efda9fa
floter1 <floter1@gmail.com>
2019-04-28 10:09:25 +0800

    update wagtail

--- news/settings.py
+++ news/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*%!e)92viptm2_j-fvd@%1x38)z*7%)!bb3-ea$h&maeq8ok%%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'floter1.pythonanywhere.com', 'localhost', '10.0.0.14', 'floter1.serveo.net']
+ALLOWED_HOSTS = [u'floter1.pythonanywhere.com', '127.0.0.1', 'localhost', '10.0.0.14', 'floter1.serveo.net']
 
 
 # Application definition
